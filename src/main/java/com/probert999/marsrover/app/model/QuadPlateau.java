c394dcc542c9702f92c8a79618885691be2bbbde
Paul Robertson <paul.robertson999@googlemail.com>
2022-01-22 09:56:18 +0000

    Plateau tests complete

--- src/main/java/com/probert999/marsrover/app/model/QuadPlateau.java
+++ src/main/java/com/probert999/marsrover/app/model/QuadPlateau.java
@@ -2,16 +2,22 @@ package com.probert999.marsrover.app.model;
 
 public class QuadPlateau extends Plateau {
 
-    private int xMaximum;
-    private int yMaximum;
+  private int xMaximum;
+  private int yMaximum;
 
-    public QuadPlateau(String plateauId, int xMaximum, int yMaximum)
-    {
-        this.plateauId = plateauId;
+  public QuadPlateau(String plateauId, int xMaximum, int yMaximum) {
+
+    if (xMaximum < 0 || yMaximum < 0) {
+      throw new IllegalArgumentException("Negative coordinates not allowed");
     }
 
-  public boolean isValidCoordinate(int x, int y) {
-    return true;
-    }
+    this.xMaximum = xMaximum;
+    this.yMaximum = yMaximum;
+    this.plateauId = plateauId;
+  }
 
+  public boolean isValidCoordinate(int xCoordinate, int yCoordinate) {
+    return ((xCoordinate >= 0 && xCoordinate <= xMaximum)
+        && (yCoordinate >= 0 && yCoordinate <= yMaximum));
+  }
 }
--- src/test/java/com/probert999/marsrover/PlateauTest.java
+++ src/test/java/com/probert999/marsrover/PlateauTest.java
@@ -3,23 +3,46 @@ package com.probert999.marsrover;
 import com.probert999.marsrover.app.model.QuadPlateau;
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PlateauTest {
 
     @Test
     public void shouldReturnCorrectPlateauId()
     {
-        // Arrange
-        String expectedResult = "QuadTestId";
-        QuadPlateau quadPlateau = new QuadPlateau(expectedResult, 5, 5);
-
-        // Act
-        String actualResult = quadPlateau.getPlateauId();
-
-        // Assert
-        assertEquals(expectedResult, actualResult);
+        String plateauId = "QuadTestId";
+        QuadPlateau quadPlateau = new QuadPlateau(plateauId, 5, 5);
+        assertEquals(plateauId, quadPlateau.getPlateauId());
     }
 
+    @Test
+    public void shouldReturnCoordinateIsValid()
+    {
+        QuadPlateau quadPlateau = new QuadPlateau("QuadTestId", 5, 5);
+        assertEquals(true, quadPlateau.isValidCoordinate(0,0));
+    }
 
+    @Test
+    public void shouldReturnCoordinateIsInValid()
+    {
+        QuadPlateau quadPlateau = new QuadPlateau("QuadTestId", 5, 5);
+        assertEquals(false, quadPlateau.isValidCoordinate(-1,0));
+    }
+
+    @Test
+    public void shouldRejectNegativeXCoordinate()
+    {
+        assertThrows(
+                IllegalArgumentException.class, () -> new QuadPlateau("QuadTestId", -5, 5));
+    }
+
+    @Test
+    public void shouldRejectNegativeYCoordinate()
+    {
+        assertThrows(
+                IllegalArgumentException.class, () -> new QuadPlateau("QuadTestId", 5, -5));
+    }
+    
 }
