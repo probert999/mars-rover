b577b5498a3090023b3ba0f4ee881cdb40b639b2
Paul Robertson <paul.robertson999@googlemail.com>
2022-02-01 15:54:07 +0000

    Improved method of instruction matching

--- src/main/java/com/probert999/marsrover/model/InstructionTypeEnum.java
+++ src/main/java/com/probert999/marsrover/model/InstructionTypeEnum.java
@@ -1,37 +1,31 @@
 package com.probert999.marsrover.model;
 
 public enum InstructionTypeEnum {
-  CREATE_PLATEAU("Create Plateau"),
-  CREATE_ROVER("Create Rover"),
-  MOVE_ROVER("Move Rover"),
-  SWITCH_PLATEAU("Switch Plateau"),
-  SWITCH_ROVER("Switch Rover"),
-  SHOW_MAP("Show Map"),
-  HIDE_MAP("Hide Map"),
+  CREATE_PLATEAU("[0-9]{1,7} [0-9]{1,7}"),
+  CREATE_ROVER("[0-9]{1,7} [0-9]{1,7} [NSEW]"),
+  MOVE_ROVER("[LRM]+"),
+  SWITCH_PLATEAU("SWITCH PLATEAU-[0-9]+"),
+  SWITCH_ROVER("SWITCH ROVER-[0-9]+"),
+  SHOW_MAP("SHOW MAP"),
+  HIDE_MAP("HIDE MAP"),
   INVALID_INSTRUCTION("Invalid Instruction");
 
+  private final String instructionText;
+
   InstructionTypeEnum(String instructionText) {
+    this.instructionText = instructionText;
   }
 
   public static InstructionTypeEnum getInstructionType(String instruction) {
 
     InstructionTypeEnum instructionType = INVALID_INSTRUCTION;
 
-    if (instruction.matches("[0-9]{1,7} [0-9]{1,7}")) {
-      instructionType = CREATE_PLATEAU;
-    } else if (instruction.matches("[0-9]{1,7} [0-9]{1,7} [NSEW]")) {
-      instructionType = CREATE_ROVER;
-    } else if (instruction.matches("[LRM]+")) {
-      instructionType = MOVE_ROVER;
-    }  else if (instruction.matches("SWITCH PLATEAU-[0-9]+")) {
-       instructionType = SWITCH_PLATEAU;
-    }  else if (instruction.matches("SWITCH ROVER-[0-9]+")) {
-        instructionType = SWITCH_ROVER;
-    }  else if (instruction.matches("SHOW MAP")) {
-        instructionType = SHOW_MAP;
-    } else if (instruction.matches("HIDE MAP")) {
-      instructionType = HIDE_MAP;
+    for (InstructionTypeEnum checkInstruction : InstructionTypeEnum.values()) {
+      if (instruction.matches(checkInstruction.instructionText)) {
+        instructionType = checkInstruction;
+        break;
+      }
     }
     return instructionType;
-  }
+    }
 }
